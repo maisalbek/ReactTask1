fb46dad56041fa1b65a6a579af223d64355a9b5e
Maisalbek <maysalbek1972@gmail.com>
2022-04-01 18:49:45 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import MyRoutes from "./MyRoutes";
+import ProductContextProvider from "./contexts/ProductContextProvider";
 
 function App() {
   return (
-    <div className="App">
-      <MyRoutes/>
-    </div>
+    <ProductContextProvider>
+      <MyRoutes />
+    </ProductContextProvider>
   );
 }
 
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Products = () => {
-    return (
-        <div>
-            <h1>Products</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Product</h1>
+    </div>
+  );
 };
 
 export default Products;
