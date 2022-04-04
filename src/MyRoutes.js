6218537f3a0268a16b0382365875897552556921
Erbol Nurmanbetov <en@MacBook-Air-Erbol.local>
2022-04-04 15:08:30 +0600

    first commit

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import {Routes, Route} from 'react-router-dom'
-import Layout from './layout/Layout';
-import Home from './pages/Home'
-import Products from './pages/Products';
-import About from './pages/About';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
 
 const MyRoutes = () => {
-    return (
-        <Routes>
-            <Route element={<Layout/>}>
-                <Route element={<Home/>}/>
-                <Route element={<About/>}/>
-                <Route element={<Products/>}/>
-            </Route>
-
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default MyRoutes;
